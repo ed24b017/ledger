9db94d355c855460b4b5a01bcaae5e6e3a0b668d
M Kaarthikeya <ed24b017@smail.iitm.ac.in>
2026-01-26 01:14:37 +0530

    react added

---
+++ ledger/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="p-6">
+				<h1 className="text-xl font-semibold">Ledger</h1>
+				<p className="text-sm text-gray-600">
+					Academic obligations, recorded.
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
